bb13ae1e848a9f81690424910d4a93f24da0f970
serena Lee <dearserenalee@gmail.com>
2019-10-28 16:00:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,7 @@
 def mario
-  phrase = "It's-a me, Mario!"
-  puts  status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
-def toadstool
-  puts status
-end
+
+
